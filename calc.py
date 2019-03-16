323dc0b938c3f6f6a19b5cfd2b0790211f8e8320
Anup Deshpande <deshpande.anup24@gmail.com>
2019-03-16 17:57:39 -0400

    Add substraction to calc.py

--- calc.py
+++ calc.py
@@ -1,13 +1,14 @@
 import add
 import div
+import substraction
 
-# calc.py
+# calc.py:
 # Main Function
 def main():
   x = input("Please input number 1: ")
   y = input("Please input number 2: ")
   print("Addition: " + add.add(x,y))
   print("Division: " + div.div(x,y)) 
-
+  print("Substraction : "+substraction.substract(x,y))  
 if __name__== "__main__":
-  main()
+    main()
