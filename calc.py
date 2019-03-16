7f4489e7109c7d2d8745871074a642c425d7769f
Sarah Gaug <sarahgaug@MacBook-Air-41.local>
2019-03-16 17:05:42 -0400

    Create add functionality

--- calc.py
+++ calc.py
@@ -1,6 +1,10 @@
+import add
 # calc.py
 # Main Function
-
+def main():
+  x = input("Please input number 1: ")
+  y = input("Please input number 2: ")
+  print(add.add(x,y))
 
 # Add function
 
@@ -15,3 +19,6 @@
 
 
 # Exponent function
+
+if __name__== "__main__":
+  main()
